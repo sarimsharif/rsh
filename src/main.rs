4c14a17ca35332597eca76c900039dcac5dbc86b
Sarim Sharif <sarimsharif@gmail.com>
2025-09-28 13:18:54 +0600

    first commit

---
+++ src/main.rs
@@ -0,0 +1,62 @@
+use std::io::Write;
+use std::path::PathBuf;
+
+fn main() {
+    let mut cwd = std::env::current_dir().unwrap();
+
+    loop {
+        let mut input = String::new();
+
+        print!("{} > ", cwd.display());
+
+        std::io::stdout().flush().unwrap();
+
+        std::io::stdin().read_line(&mut input).expect("err");
+        
+        let parts = input.trim().split(" ").map(|v| v.to_string()).collect::<Vec<String>>();
+
+        if parts.len() == 0 { continue; }
+
+        match parts[0].as_str() {
+            "glungus" => {
+                println!("🐱");
+            }
+            "exit" => break,
+            "" => {}
+            "ls" => {
+                let dir_contents = std::fs::read_dir(&cwd).unwrap();
+                for entry in dir_contents {
+                    println!(
+                        "{}",
+                        entry
+                            .expect("reason")
+                            .path()
+                            .iter()
+                            .last()
+                            .unwrap()
+                            .display()
+                    );
+                }
+            }
+            "cd" => {
+                let new_path = cwd.join(parts.get(1).expect("usage: cd [dir]"));
+
+                if !new_path.is_dir() {
+                    panic!("it isnt a directory!!");
+                }
+                std::env::set_current_dir(&new_path).unwrap();
+
+                cwd = new_path;
+            },
+
+            "clear" => {
+                print!("{esc}[2J{esc}[1;1H", esc = 27 as char); 
+                std::io::stdout().flush();
+            },
+
+            _ => {
+                println!("rsh: command not found: {input}");
+            }
+        }
+    }
+}
